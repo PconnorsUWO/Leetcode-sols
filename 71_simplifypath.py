79d0b8c48cc85d45f868868815dc3febce877300
pconnors@uwo.ca <124845606+PconnorsUWO@users.noreply.github.com>
2025-01-25 15:29:26 -0500

    71 simplifypath

---
+++ 71_simplifypath.py
@@ -0,0 +1,33 @@
+# starts with /
+# aa single period represents the cur dir
+# a double preiod represents the prev dir
+# multiple consecutive slashes are treatred as a single /
+# ......... are valid dir names
+
+
+def simplifyPath(path: str) -> str:
+    path = path.split("/")
+    res = ""
+    for i in path:
+        if i == "":
+            continue
+        elif i == ".." or i == ".":
+            while res and res[-1] != "/":
+                res = res[:-1]
+            res = res[:-1]
+            continue
+        text = "/" + i
+        res += text
+
+    if res == "":
+        return "/"
+    
+    return res
+
+case1 = "/home/"
+case2 = "/home/user/Documents/../Pictures"
+case3 = "/home/user/../../"
+
+print(simplifyPath(case1))
+print(simplifyPath(case2))
+print(simplifyPath(case3))
