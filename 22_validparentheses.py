cd272aeff157a3735653b9ac3bcdc5733b8db0cd
pconnors@uwo.ca <124845606+PconnorsUWO@users.noreply.github.com>
2025-01-30 17:38:34 -0500

    valid parentheses

---
+++ 22_validparentheses.py
@@ -0,0 +1,29 @@
+from typing import List
+def generateParenthesis(n: int) -> List[str]:
+    if n == 1:
+        return ["()"]
+    s = ""
+    res = []
+
+    def backtrack(s, left, right):
+        if len(s) == 2*n:
+            res.append(s)
+            return
+        if left < n:
+            backtrack(s + "(", left + 1, right)
+        if right < left:
+            backtrack(s + ")", left, right + 1)
+    backtrack(s, 0, 0)
+    return res
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+# Example 2:
+
+# Input: n = 1
+# Output: ["()"]
+
+case1 = 3
+print(generateParenthesis(case1))
